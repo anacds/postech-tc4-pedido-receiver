3b352a702029516181a0a3f751e53203426282bb
Ana Silva <anacxx@live.com>
2025-05-02 17:39:23 -0300

    Setup inicial do projeto

---
+++ src/main/java/com/example/postech_tc4_pedido_receiver/PostechTc4PedidoReceiverApplication.java
@@ -0,0 +1,13 @@
+package com.example.postech_tc4_pedido_receiver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostechTc4PedidoReceiverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostechTc4PedidoReceiverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/postech_tc4_pedido_receiver/PostechTc4PedidoReceiverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.postech_tc4_pedido_receiver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostechTc4PedidoReceiverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
